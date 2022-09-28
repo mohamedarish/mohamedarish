30307756da1fa1ad2a4d55ea6dd98b53beb073a6
Mohamed Arish T P <arish1248@gmail.com>
2022-09-28 10:55:02 +0530

    added the devlog and also added the theme-switcher

---
+++ devLog.md
@@ -0,0 +1,4 @@
+# DevLog
+
+- 29/9:  The first update is a bit late but Now, I plan on doing the devlog for this repo daily
+  - update 1: Added the theme-switcher and it was easier to implement than what I expected, Looking forward to adding more content to the website.
--- src/script.ts
+++ src/script.ts
@@ -35,22 +35,80 @@ document.addEventListener('DOMContentLoaded', function (event) {
 	}
 	StartTextAnimation(0);
 
+	const theme = localStorage.getItem("theme");
+
 	const docBody = document.body;
 	const githubImage = document.getElementById("github");
 	const gitlabImage = document.getElementById("gitlab");
 	const mailImage = document.getElementById("mail");
+	const themeSwitcher = document.getElementById("themeSwitcher");
 
 	if (!githubImage) return;
 	if (!gitlabImage) return;
 	if (!mailImage) return;
+	if (!themeSwitcher) return;
+
+	if (theme) {
+		if (!docBody.classList.contains(theme)) {
+			if (theme == "dark") {
+				docBody.classList.remove("light");
+				docBody.classList.add("dark");
+			} else if (theme == "light") {
+				docBody.classList.remove("dark");
+				docBody.classList.add("light");
+			}
+		}
+	} else {
+		docBody.classList.add("light");
+	}
 
 	if (docBody.classList.contains("dark")) {
 		githubImage.setAttribute("src", "./icons/github-dark.png");
 		gitlabImage.setAttribute("src", "./icons/gitlab-dark.png");
 		mailImage.setAttribute("src", "./icons/email-dark.png");
+		themeSwitcher.setAttribute("src", "./icons/moon.png");
 	} else if (docBody.classList.contains("light")) {
 		githubImage.setAttribute("src", "./icons/github-light.png");
 		gitlabImage.setAttribute("src", "./icons/gitlab-light.png");
 		mailImage.setAttribute("src", "./icons/email-light.png");
+		themeSwitcher.setAttribute("src", "./icons/sun.png");
 	}
 });
+
+
+const themeSwitcher = document.getElementById("themeSwitcher");
+
+if (themeSwitcher) {
+
+	themeSwitcher.onclick = () => {
+		const bodyElement = document.body;
+
+		const githubImage = document.getElementById("github");
+		const gitlabImage = document.getElementById("gitlab");
+		const mailImage = document.getElementById("mail");
+
+		if (!bodyElement) return;
+		if (!githubImage) return;
+		if (!gitlabImage) return;
+		if (!mailImage) return;
+
+		if (bodyElement.classList.contains("dark")) {
+			bodyElement.classList.remove("dark");
+			bodyElement.classList.add("light");
+
+			githubImage.setAttribute("src", "./icons/github-light.png");
+			gitlabImage.setAttribute("src", "./icons/gitlab-light.png");
+			mailImage.setAttribute("src", "./icons/email-light.png");
+			themeSwitcher.setAttribute("src", "./icons/sun.png");
+		} else if (bodyElement.classList.contains("light")) {
+			bodyElement.classList.remove("light");
+			bodyElement.classList.add("dark");
+			githubImage.setAttribute("src", "./icons/github-dark.png");
+			gitlabImage.setAttribute("src", "./icons/gitlab-dark.png");
+			mailImage.setAttribute("src", "./icons/email-dark.png");
+			themeSwitcher.setAttribute("src", "./icons/moon.png");
+		}
+
+		localStorage.setItem("theme", bodyElement.classList.contains("light") ? "light" : "dark");
+	}
+};
